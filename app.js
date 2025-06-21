fe199d95bf8a735d7dff0b48df44f51b05819eed
Andres Diaz <anondiaz@gmail.com>
2025-06-21 21:35:15 +0200

    In progress

--- app.js
+++ app.js
@@ -71,31 +71,42 @@
 // http://localhost:4000/asignatura <- alumnos de una asignatura
 
 // ------------------------------------------------------------------------------------
-// Aquí empezamos con el programa:
+// Aquí empezamos con el programa parte web:
 // ------------------------------------------------------------------------------------
 // Opciones:
 // Vacio -> Listado de alumnos
 // Asignatura -> Alumnos de una asignatura
 // Nombre y apellido -> Datos de un alumno
-// Nombre, apellido y -1 -> Borrar un alumno
-// Nombre, apellido, edad y asignatura -> Matricular un alumno
 // ------------------------------------------------------------------------------------
 
 //Dependencias
 const express = require('express');
 const app = express();
 const fs = require('node:fs');
+const path = require("node:path");
 
 // Establecemos el puerto de la aplicación
-process.loadEnvFile()
-const PORT = process.env.PORT || 4000;
+// process.loadEnvFile()
+const PORT = 4000; // || process.env.PORT;
+
+//Middleware para parsear el cuerpo de las peticiones
+app.use(express.urlencoded({ extended: true })); // Middleware para parsear datos de formularios
+app.use(express.json()); // Middleware para parsear JSON
+
+//Ruta de acceso a los archivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+// Vistas
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //Cargamos el fichero de datos
-const escuela = fs.readFileSync('escuela.json', 'utf-8');
-const escuelaData = JSON.parse(escuela);
+// const escuela = fs.readFileSync('escuela.json', 'utf-8');
+const escuelaData = require("./escuela.json");
 
 
-app.get('/', (req, res) => {
+// Peticion de todos los alumnos
+app.get('/api', (req, res) => {
     // =========================
     // Listado de alumnos via web
     // =========================
@@ -109,99 +120,185 @@ app.get('/', (req, res) => {
         }
         return a.apellido.localeCompare(b.apellido);
     });
-    // Mostramos los datos de los alumnos
-    escuelaData.forEach(alumno => {
-        console.log(`${alumno.nombre} ${alumno.apellido} ${alumno.edad} ${alumno.asignatura}`);
-    });
-    console.log("\n" + "-".repeat(tituloProceso.length - 1) + "\n");
-    console.log(`Total: ${escuelaData.length} alumnos matriculados\n`);
-    // console.log(escuelaData);
-    // console.log(process.argv.length);
-    process.exit(1);
-
+    // Mostramos los datos de los alumnos en el servidor web en formato JSON
+    // console.log(escuelaData);    
+    res.json(escuelaData);
 });
 
 
-
-app.get('/:asignatura', (req, res) => {
-
+// Petición de alumnos por asignatura
+app.get('/api/:asignatura', (req, res) => {
     // =========================
     // Alumnos de la asignatura via web
     // =========================
-    // Comprobamos si el argumento es una asignatura valida
-    if (!escuelaData.some(alumno => alumno.asignatura === argumento1)) {
-            // Montamos el titulo del proceso
-        tituloProceso = `\nNo tenemos matriculado a ningún alumno en ${argumento1} :`;
-        establecerTitulo(tituloProceso);
-        // console.log(process.argv.length);
-        //vamos a mostrar las asignaturas disponibles
-        console.log("Asignaturas disponibles :\n");
-        const asignaturas = [];
-        escuelaData.forEach(alumno => {
-            let asignaturaSeleccionada = alumno.asignatura;
-            // Comprobamos si la asignatura ya está en el array
-            if (asignaturas.includes(asignaturaSeleccionada)) {
-                return; // Si ya está, no la añadimos
-            }
-            asignaturas.push(asignaturaSeleccionada);
-        });
-        // console.log(asignaturas);
-        asignaturas.forEach(asignatura => {
-            console.log(`- ${asignatura}`);
-        });
-        process.exit(1);
+    // Obtenemos la asignatura de los parámetros de la URL
+    const asignatura = req.params.asignatura;
+
+    // Comprobamos si la asignatura existe en la lista de alumnos
+    const asignaturaExistente = escuelaData.some(alumno => alumno.asignatura === asignatura);
+
+    // Filtramos los alumnos por la asignatura
+    const alumnosAsignatura = escuelaData.filter(alumno => alumno.asignatura === asignatura);
+    
+    // Vamos a filtrar para obtener todas las asignaturas unitariamente
+    // Esto nos servirá para mostrar las asignaturas disponibles si la solicitada no existe
+    const asignaturas = [];
+    escuelaData.forEach(alumno => {
+        let asignaturaSeleccionada = alumno.asignatura;
+        // Comprobamos si la asignatura ya está en el array
+        if (asignaturas.includes(asignaturaSeleccionada)) {
+            return; // Si ya está, no la añadimos
+        }
+        // Si no está, la añadimos al array
+        asignaturas.push(asignaturaSeleccionada);
+    });
+
+    // Si la asignatura no existe, mostramos un mensaje
+    if (!asignaturaExistente) {
+        // Si la asignatura no existe, mostramos el listado de asignaturas
+        return res.status(404).send(`No existe la asignatura ${asignatura}. <br> Las asignaturas disponibles son: <br> ${asignaturas .join('<br>')}`);
     }
 
-    // Montamos el titulo del proceso
-    tituloProceso = `\nAlumnos matriculados en ${argumento1} :`;
-    establecerTitulo(tituloProceso);
-    // Filtramos los alumnos por asignatura
-    alumnosMatriculados = 0;
-    escuelaData.forEach(alumno => {
-        if (alumno.asignatura === argumento1) {
-            console.log(`${alumno.nombre} ${alumno.apellido} ${alumno.edad}`);
-            alumnosMatriculados++;
-        }
-    });
-    console.log("\n" + "-".repeat(tituloProceso.length - 1) + "\n");
-    console.log(`Total: ${alumnosMatriculados} alumnos matriculados\n`);
-    // console.log(process.argv.length);
-    
+    // Si no hay alumnos en la asignatura, mostramos un mensaje
+    if (alumnosAsignatura.length === 0) {
+        return res.status(404).send(`No hay alumnos matriculados en la asignatura ${asignatura}`);
+    }
 
+    // Ordenamos los alumnos por apellido, nombre y edad
+    alumnosAsignatura.sort((a, b) => {
+        if (a.apellido === b.apellido) {
+            if (a.nombre === b.nombre) {
+                return a.edad - b.edad;
+            }
+            return a.nombre.localeCompare(b.nombre);
+        }
+        return a.apellido.localeCompare(b.apellido);
+    });
+
+    // Mostramos los datos de los alumnos en la asignatura en formato JSON
+    // console.log(alumnosAsignatura);
+    res.json({
+        // Nombre de la asignatura
+        asignatura: asignatura,
+        // Alumnos matriculados en la asignatura
+        alumnos: alumnosAsignatura,
+        // Total de alumnos matriculados en la asignatura
+        total: alumnosAsignatura.length
+    });    
 });
 
 
-app.get('/:nombre/:apellido', (req, res) => {
+app.get('/api/:nombre/:apellido', (req, res) => {
     // =========================
     // Datos de alumno via web
     // =========================
-    // Comprobamos si el alumno está matriculado
-    const alumno = escuelaData.find(alumno => 
-        alumno.nombre === argumento1 && alumno.apellido === argumento2
-    );
-    if (!alumno) {
-        // Montamos el titulo del proceso
-        tituloProceso = `\nNo tenemos matriculado a ese alumno :`;
-        establecerTitulo(tituloProceso);
-        console.log(`${argumento1} ${argumento2} no está matriculado`);
-        process.exit(1);
-    }
-    // Montamos el titulo del proceso
-    //El alumno nombre_alumno apellido_alumno
-    tituloProceso = `\nEl alumno ${argumento1} ${argumento2} está matriculado de :`;
-    establecerTitulo(tituloProceso);
-
-    escuelaData.forEach(alumno => {
-        if (alumno.nombre == argumento1 && alumno.apellido == argumento2) {
-            console.log(`-- ${alumno.asignatura}`);
-        }
-    });
+    // Obtenemos el nombre y apellido de los parámetros de la URL
+    const { nombre, apellido } = req.params;
     
+    // Buscamos el alumno en la lista con find por simplicidad
+    // y filtramos las asignaturas del alumno con filter
+    const alumno = escuelaData.find(alumno => alumno.nombre === nombre && alumno.apellido === apellido);
+    const asignaturasAlumno = escuelaData.filter(alumno => alumno.nombre === nombre && alumno.apellido === apellido)
 
+    // Si no encontramos al alumno, mostramos un mensaje
+    if (!alumno) {
+        return res.status(404).send(`No tenemos matriculado a ese alumno ${nombre} ${apellido}`);
+    }
+    // Mostramos los datos del alumno
+    // console.log(asignaturasAlumno);
+    res.json({asignaturasAlumno
+    }); 
+});
 
+app.get('/', (req, res) => {
+    // =========================
+    // Página de inicio
+    // =========================
+    res.render('root', {escuelaData});
+});
+
+app.get('/:asignatura', (req, res) => {
+    // =========================
+    // Página de asignatura
+    // =========================
+    // Obtenemos la asignatura de los parámetros de la URL
+    const asignatura = req.params.asignatura;
+    // Comprobamos si la asignatura existe en la lista de alumnos
+    const asignaturaExistente = escuelaData.some(alumno => alumno.asignatura === asignatura);
+    // Filtramos los alumnos por la asignatura
+    const alumnosAsignatura = escuelaData.filter(alumno => alumno.asignatura === asignatura);
+    // Vamos a filtrar para obtener todas las asignaturas unitariamente
+    // Esto nos servirá para mostrar las asignaturas disponibles si la solicitada no existe
+    const asignaturas = [];
+    escuelaData.forEach(alumno => {
+        let asignaturaSeleccionada = alumno.asignatura;
+        // Comprobamos si la asignatura ya está en el array
+        if (asignaturas.includes(asignaturaSeleccionada)) {
+            return; // Si ya está, no la añadimos
+        }
+        // Si no está, la añadimos al array
+        asignaturas.push(asignaturaSeleccionada);
+    });
+    // Si la asignatura no existe, mostramos un mensaje
+    if (!asignaturaExistente) {
+        // Si la asignatura no existe, mostramos el listado de asignaturas
+        return res.status(404).send(`No existe la asignatura ${asignatura}. <br> Las asignaturas disponibles son: <br> ${asignaturas.join('<br>')}`);
+    }
+    // Si no hay alumnos en la asignatura, mostramos un mensaje
+    if (alumnosAsignatura.length === 0) {
+        return res.status(404).send(`No hay alumnos matriculados en la asignatura ${asignatura}`);
+    }
+    // Ordenamos los alumnos por apellido, nombre y edad
+    alumnosAsignatura.sort((a, b) => {
+        if (a.apellido === b.apellido) {
+            if (a.nombre === b.nombre) {
+                return a.edad - b.edad;
+            }
+            return a.nombre.localeCompare(b.nombre);
+        }
+        return a.apellido.localeCompare(b.apellido);
+    });
+    // Mostramos los datos de los alumnos en la asignatura en formato JSON
+    // console.log(alumnosAsignatura);
+    // console.log(asignatura);
+    
+    res.render('asignatura', {alumnosAsignatura, asignatura});//, asignatura, total: alumnosAsignatura.length});
+});
+
+// Ruta para obtener los datos de un alumno por nombre y apellido
+app.get('/:nombre/:apellido', (req, res) => {
+    // =========================
+    // Página de alumno
+    // =========================
+    // Obtenemos el nombre y apellido de los parámetros de la URL
+    const { nombre, apellido } = req.params;
+    
+    // Buscamos el alumno en la lista con find por simplicidad
+    // y filtramos las asignaturas del alumno con filter
+    const alumno = escuelaData.find(alumno => alumno.nombre === nombre && alumno.apellido === apellido);
+    const asignaturasAlumno = escuelaData.filter(alumno => alumno.nombre === nombre && alumno.apellido === apellido);
+
+    // Si no encontramos al alumno, mostramos un mensaje
+    if (!alumno) {
+        return res.status(404).send(`No tenemos matriculado a ese alumno ${nombre} ${apellido}`);
+    }
+    
+    // Mostramos los datos del alumno
+    // console.log(asignaturasAlumno);
+    
+    res.render('alumno', {asignaturasAlumno, nombre, apellido});
+    
+});
+
+// Manejo de errores 404 por si la ruta no existe
+app.use((req, res) => {
+    // =========================
+    // Error 404
+    // =========================
+    // Si la ruta no existe, mostramos un mensaje de error
+    res.status(404).send('Error: Ruta no encontrada');
 });
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
 });
-
